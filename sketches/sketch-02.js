e6e6d7d92ff0efa91efda0ce2f896e075930a662
Krzysztof Baranski <krisbaranski@gmail.com>
2022-03-15 21:59:06 +0100

    feat: Transform/translate context object

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -16,9 +16,20 @@ const sketch = () => {
     const w = width * 0.3
     const h = height * 0.3
 
+    context.save()
+    context.translate(x, y)
+    context.rotate(0.3)
+
     context.beginPath()
-    context.rect(x, y, w, h)
+    context.rect(-w * 0.5, -h * 0.5, w, h)
     context.fill()
+    context.restore()
+
+    // context.translate(100, 400)
+
+    // context.beginPath()
+    // context.arc(0, 0, 50, 0, Math.PI * 2)
+    // context.fill()
   }
 }
 
