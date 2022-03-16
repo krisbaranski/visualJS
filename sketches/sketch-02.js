ce89365fb580a1b38eda26ea393f5f8283c8c7fb
Krzysztof Baranski <krisbaranski@gmail.com>
2022-03-16 20:00:01 +0100

    feat: Add utility for commonly used functions

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -1,12 +1,18 @@
 const canvasSketch = require("canvas-sketch")
+const math = require("canvas-sketch-util/math")
+const random = require("canvas-sketch-util/random")
 
 const settings = {
   dimensions: [1080, 1080],
 }
 
-const degreesToRadians = degrees => {
-  return (degrees * Math.PI) / 180
-}
+// const degreesToRadians = degrees => {
+//   return (degrees * Math.PI) / 180
+// }
+
+// const randomRange = (min, max) => {
+//   return Math.random() * (max - min) + min
+// }
 
 const sketch = () => {
   return ({ context, width, height }) => {
@@ -26,7 +32,7 @@ const sketch = () => {
     const radius = width * 0.3
 
     for (let i = 0; i < num; i++) {
-      const slice = degreesToRadians(360 / num)
+      const slice = math.degToRad(360 / num)
       const angle = slice * i
 
       x = cx + Math.sin(angle) * radius
@@ -35,6 +41,7 @@ const sketch = () => {
       context.save()
       context.translate(x, y)
       context.rotate(-angle)
+      context.scale(random.range(1, 3), 1)
 
       context.beginPath()
       context.rect(-w * 0.5, -h * 0.5, w, h)
