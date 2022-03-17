8fa45f33c3fc52746f20db1222d15df3a5c1ce84
Krzysztof Baranski <krisbaranski@gmail.com>
2022-03-17 20:12:02 +0100

    feat: Animate points

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -3,12 +3,20 @@ const random = require("canvas-sketch-util/random")
 
 const settings = {
   dimensions: [1080, 1080],
+  animate: true,
 }
+
+// const animate = () => {
+//   console.log("domenstika")
+//   requestAnimationFrame(animate)
+// }
+
+// animate()
 
 const sketch = ({ context, width, height }) => {
   const agents = []
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 50; i++) {
     const x = random.range(0, width)
     const y = random.range(0, height)
 
@@ -19,14 +27,16 @@ const sketch = ({ context, width, height }) => {
     context.fillRect(0, 0, width, height)
 
     agents.forEach(agent => {
+      agent.update()
       agent.draw(context)
+      agent.bounce(width, height)
     })
   }
 }
 
 canvasSketch(sketch, settings)
 
-class Point {
+class Vector {
   constructor(x, y) {
     this.x = x
     this.y = y
@@ -35,15 +45,36 @@ class Point {
 
 class Agent {
   constructor(x, y) {
-    this.pos = new Point(x, y)
-    this.radius = 10
+    this.pos = new Vector(x, y)
+    this.vel = new Vector(random.range(-1, 1), random.range(-1, 1))
+    this.radius = random.range(4, 12)
+  }
+
+  bounce(width, height) {
+    if (this.pos.x <= 0 || this.pos.x >= width) {
+      this.vel.x *= -1
+    }
+    if (this.pos.y <= 0 || this.pos.y >= width) {
+      this.vel.y *= -1
+    }
+  }
+
+  update() {
+    this.pos.x += this.vel.x
+    this.pos.y += this.vel.y
   }
 
   draw(context) {
-    context.fillStyle = "black"
+    context.save()
+    context.translate(this.pos.x, this.pos.y)
+
+    context.lineWidth = 3
 
     context.beginPath()
-    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2)
+    context.arc(0, 0, this.radius, 0, Math.PI * 2)
     context.fill()
+    context.stroke()
+
+    context.restore()
   }
 }
