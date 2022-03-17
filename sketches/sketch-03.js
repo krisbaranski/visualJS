0087d906a43fe05b57d5c81c4fede12a429f6d34
Krzysztof Baranski <krisbaranski@gmail.com>
2022-03-17 20:44:35 +0100

    feat: Add lines between points, change lineWidth with distance

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -1,5 +1,6 @@
 const canvasSketch = require("canvas-sketch")
 const random = require("canvas-sketch-util/random")
+const math = require("canvas-sketch-util/math")
 
 const settings = {
   dimensions: [1080, 1080],
@@ -16,7 +17,7 @@ const settings = {
 const sketch = ({ context, width, height }) => {
   const agents = []
 
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 40; i++) {
     const x = random.range(0, width)
     const y = random.range(0, height)
 
@@ -25,6 +26,25 @@ const sketch = ({ context, width, height }) => {
   return ({ context, width, height }) => {
     context.fillStyle = "white"
     context.fillRect(0, 0, width, height)
+
+    for (let i = 0; i < agents.length; i++) {
+      const agent = agents[i]
+
+      for (let j = i + 1; j < agents.length; j++) {
+        const other = agents[j]
+
+        const dist = agent.pos.getDistance(other.pos)
+
+        if (dist > 300) continue
+
+        context.lineWidth = math.mapRange(dist, 0, 300, 4, 0.1)
+
+        context.beginPath()
+        context.moveTo(agent.pos.x, agent.pos.y)
+        context.lineTo(other.pos.x, other.pos.y)
+        context.stroke()
+      }
+    }
 
     agents.forEach(agent => {
       agent.update()
@@ -41,13 +61,19 @@ class Vector {
     this.x = x
     this.y = y
   }
+
+  getDistance(v) {
+    const dx = this.x - v.x
+    const dy = this.y - v.y
+    return Math.sqrt(dx * dx + dy * dy)
+  }
 }
 
 class Agent {
   constructor(x, y) {
     this.pos = new Vector(x, y)
     this.vel = new Vector(random.range(-1, 1), random.range(-1, 1))
-    this.radius = random.range(4, 12)
+    this.radius = random.range(3, 12)
   }
 
   bounce(width, height) {
@@ -68,7 +94,7 @@ class Agent {
     context.save()
     context.translate(this.pos.x, this.pos.y)
 
-    context.lineWidth = 3
+    context.lineWidth = 4
 
     context.beginPath()
     context.arc(0, 0, this.radius, 0, Math.PI * 2)
