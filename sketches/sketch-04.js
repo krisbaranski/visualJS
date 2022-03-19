947af94264d8c87586a8fbcfc99795e3f2b7f2c6
Krzysztof Baranski <krisbaranski@gmail.com>
2022-03-19 12:50:44 +0100

    feat: Create control panel, add paremeters

--- sketches/sketch-04.js
+++ sketches/sketch-04.js
@@ -1,10 +1,22 @@
 const canvasSketch = require("canvas-sketch")
 const random = require("canvas-sketch-util/random")
 const math = require("canvas-sketch-util/math")
+const tweakpane = require("tweakpane")
 
 const settings = {
   dimensions: [1080, 1080],
   animate: true,
+}
+const params = {
+  cols: 10,
+  rows: 10,
+  scaleMin: 0.1,
+  scaleMax: 1,
+  freq: 0.001,
+  amp: 0.2,
+  animate: true,
+  frame: 0,
+  lineCap: "butt",
 }
 
 const sketch = () => {
@@ -12,12 +24,12 @@ const sketch = () => {
     context.fillStyle = "white"
     context.fillRect(0, 0, width, height)
 
-    const cols = 10
-    const rows = 10
+    const cols = params.cols
+    const rows = params.rows
     const numCells = cols * rows
 
-    const gridWidth = width * 0.8
-    const gridHeight = height * 0.8
+    const gridWidth = width * 0.95
+    const gridHeight = height * 0.95
     const cellWidth = gridWidth / cols
     const cellHeight = gridHeight / rows
     const marginWidth = (width - gridWidth) / 2
@@ -29,15 +41,25 @@ const sketch = () => {
 
       const x = col * cellWidth
       const y = row * cellHeight
-      const width = cellWidth * 0.8
-      const height = cellHeight * 0.8
+      const width = cellWidth * 0.9
+      const height = cellHeight * 0.9
 
-      const noise = random.noise2D(x + frame * 10, y, 0.001)
-      const angle = noise * Math.PI * 0.2
+      const f = params.animate ? frame : params.frame
+
+      // const noise = random.noise2D(x + frame * 10, y, params.freq)
+      const noise = random.noise3D(x, y, f * 10, params.freq)
+
+      const angle = noise * Math.PI * params.amp
 
       // const scale = (noise + 1) / 2 * 30
       // const scale = (noise * 0.5 + 0.5) * 30
-      const scale = math.mapRange(noise, -1, 1, 1, 30)
+      const scale = math.mapRange(
+        noise,
+        -1,
+        1,
+        params.scaleMin,
+        params.scaleMax
+      )
 
       context.save()
       context.translate(x, y)
@@ -46,6 +68,7 @@ const sketch = () => {
       context.rotate(angle)
 
       context.lineWidth = scale
+      context.lineCap = params.lineCap
 
       context.beginPath()
       context.moveTo(width * -0.5, 0)
@@ -57,4 +80,25 @@ const sketch = () => {
   }
 }
 
+const createPane = () => {
+  const pane = new tweakpane.Pane()
+  let folder
+
+  folder = pane.addFolder({ title: "Grid" })
+  folder.addInput(params, "lineCap", {
+    options: { butt: "butt", round: "round", square: "square" },
+  })
+  folder.addInput(params, "cols", { min: 2, max: 60, step: 1 })
+  folder.addInput(params, "rows", { min: 2, max: 60, step: 1 })
+  folder.addInput(params, "scaleMin", { min: 1, max: 100 })
+  folder.addInput(params, "scaleMax", { min: 1, max: 100 })
+
+  folder = pane.addFolder({ title: "Noise" })
+  folder.addInput(params, "freq", { min: -0.01, max: 0.01 })
+  folder.addInput(params, "amp", { min: 0, max: 1 })
+  folder.addInput(params, "animate")
+  folder.addInput(params, "frame", { min: 0, max: 100 })
+}
+
+createPane()
 canvasSketch(sketch, settings)
